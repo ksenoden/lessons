e5d0f4b9140262c5d3048fdf2b4357947f98b04f
ilya682003 <ilyanfs682003@mail.ru>
2017-07-02 11:33:49 +0300

    Добавили .gitignore

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ $name1 = $_POST["imja"];
 $age = $_POST["vozr"];
 $myclass = $_POST["class"];
 
-echo "Меня зовут ".$name1." и мне ".$age." лет и я учусь в ".$myclass." классе.";
+echo "Привет, меня зовут ".$name1." и мне ".$age." лет и я учусь в ".$myclass." классе.";
 
 
