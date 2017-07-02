6798d58be6525817d2d59dea406b20bab61c5c69
ilya682003 <ilyanfs682003@mail.ru>
2017-07-02 11:44:38 +0300

    Сделали ДЗ

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ $name1 = $_POST["imja"];
 $age = $_POST["vozr"];
 $myclass = $_POST["class"];
 
-echo "Привет, меня зовут ".$name1." и мне ".$age." лет и я учусь в ".$myclass." классе.";
+echo "Привет медвед, меня зовут ".$name1." и мне ".$age." лет и я учусь в ".$myclass." классе.";
 
 
